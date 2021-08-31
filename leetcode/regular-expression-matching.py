b2fcaa04c49cefa5b959ffb289d1ad49b85d5cb4
Benjamin Stammen <benjamin@stammen.me>
2021-08-30 22:43:27 -0700

    Uncomment unit tests for regex problem

--- leetcode/regular-expression-matching.py
+++ leetcode/regular-expression-matching.py
@@ -49,51 +49,51 @@ class Solution(object):
 
 
 class TestSolution(unittest.TestCase):
-    # def test_none_0(self):
-    #     s = ""
-    #     p = ""
-    #     self.assertTrue(Solution().isMatch(s, p))
+    def test_none_0(self):
+        s = ""
+        p = ""
+        self.assertTrue(Solution().isMatch(s, p))
 
-    # def test_none_1(self):
-    #     s = ""
-    #     p = "a"
-    #     self.assertFalse(Solution().isMatch(s, p))
+    def test_none_1(self):
+        s = ""
+        p = "a"
+        self.assertFalse(Solution().isMatch(s, p))
 
-    # def test_no_symbol_equal(self):
-    #     s = "abcd"
-    #     p = "abcd"
-    #     self.assertTrue(Solution().isMatch(s, p))
+    def test_no_symbol_equal(self):
+        s = "abcd"
+        p = "abcd"
+        self.assertTrue(Solution().isMatch(s, p))
 
-    # def test_no_symbol_not_equal_0(self):
-    #     s = "abcd"
-    #     p = "efgh"
-    #     self.assertFalse(Solution().isMatch(s, p))
+    def test_no_symbol_not_equal_0(self):
+        s = "abcd"
+        p = "efgh"
+        self.assertFalse(Solution().isMatch(s, p))
 
-    # def test_no_symbol_not_equal_1(self):
-    #     s = "ab"
-    #     p = "abb"
-    #     self.assertFalse(Solution().isMatch(s, p))
+    def test_no_symbol_not_equal_1(self):
+        s = "ab"
+        p = "abb"
+        self.assertFalse(Solution().isMatch(s, p))
 
-    # def test_symbol_0(self):
-    #     s = ""
-    #     p = "a*b*"
-    #     self.assertTrue(Solution().isMatch(s, p))
+    def test_symbol_0(self):
+        s = ""
+        p = "a*b*"
+        self.assertTrue(Solution().isMatch(s, p))
 
-    # def test_symbol_1(self):
-    #     s = "a"
-    #     p = "ab*"
-    #     self.assertTrue(Solution().isMatch(s, p))
+    def test_symbol_1(self):
+        s = "a"
+        p = "ab*"
+        self.assertTrue(Solution().isMatch(s, p))
 
-    # def test_symbol_2(self):
-    #     # E.g.
-    #     #   s a b b
-    #     # p 1 0 0 0
-    #     # a 0 1 0 0
-    #     # b 0 0 1 0
-    #     # * 0 1 1 1
-    #     s = "abb"
-    #     p = "ab*"
-    #     self.assertTrue(Solution().isMatch(s, p))
+    def test_symbol_2(self):
+        # E.g.
+        #   s a b b
+        # p 1 0 0 0
+        # a 0 1 0 0
+        # b 0 0 1 0
+        # * 0 1 1 1
+        s = "abb"
+        p = "ab*"
+        self.assertTrue(Solution().isMatch(s, p))
 
 
 if __name__ == "__main__":
