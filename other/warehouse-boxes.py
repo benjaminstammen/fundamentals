9784f4e1abcb68e5dd4f989b9df3be714424bfda
Benjamin Stammen <benjamin@stammen.me>
2021-08-31 18:27:35 -0700

    Add nlog(n) solution to warehouse problem

--- other/warehouse-boxes.py
+++ other/warehouse-boxes.py
@@ -2,7 +2,7 @@ import unittest
 from typing import List
 
 class Solution(object):
-    def get_num_boxes(self, boxes: List[int]):
+    def get_num_boxes_n_squared(self, boxes: List[int]):
         empty_boxes = []
         # sort using largest first so that
         # we're not putting a tiny box into a
@@ -23,38 +23,53 @@ class Solution(object):
             empty_boxes.append(box)
         return len(empty_boxes)
 
+    def get_num_boxes_n_log_n(self, boxes: List[int]):
+        boxes.sort() # sorting takes n * log(n), remainder takes n
+        num_boxes = len(boxes)
+        l, r = 0, 1
+        while r < len(boxes):
+            if boxes[l] <= boxes[r] / 2:
+                num_boxes -= 1
+                l += 1
+                r += 1
+            else:
+                r += 1
+        return num_boxes
+        
+
+
 
 class TestSolution(unittest.TestCase):
 
     def test_1(self):
+        boxes = [5, 2, 9, 7, 8, 6, 2, 4]
         self.assertEqual(
-            Solution().get_num_boxes(
-                boxes=[5, 2, 9, 7, 8, 6, 2, 4],
-            ),
+            Solution().get_num_boxes_n_squared(boxes),
+            Solution().get_num_boxes_n_log_n(boxes),
             5
         )
 
     def test_2(self):
+        boxes = [2, 2, 2, 2, 2]
         self.assertEqual(
-            Solution().get_num_boxes(
-                boxes=[2, 2, 2, 2, 2],
-            ),
+            Solution().get_num_boxes_n_squared(boxes),
+            Solution().get_num_boxes_n_log_n(boxes),
             5
         )
 
     def test_3(self):
+        boxes = [25, 50, 100]
         self.assertEqual(
-            Solution().get_num_boxes(
-                boxes=[25, 50, 100],
-            ),
+            Solution().get_num_boxes_n_squared(boxes),
+            Solution().get_num_boxes_n_log_n(boxes),
             1
         )
     
     def test_4(self):
+        boxes = [1, 1, 6, 3]
         self.assertEqual(
-            Solution().get_num_boxes(
-                boxes=[1, 1, 6, 3],
-            ),
+            Solution().get_num_boxes_n_squared(boxes),
+            Solution().get_num_boxes_n_log_n(boxes),
             2
         )
     
